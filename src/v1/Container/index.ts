57459310b913c37c0aaaff87a7ea6102fa0c9be8
misterJS <64055552+misterJS@users.noreply.github.com>
2023-12-06 03:41:40 +0700

    Delete src directory

--- src/v1/Container/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Container";
-export * from "./Container.type";
-export * from "./Container.module.css";
--- src/v1/Typography/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Typography";
-export * from "./Typography.type";
-export * from "./Typography.module.css";
